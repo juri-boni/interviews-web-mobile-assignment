a845bf6d52f31e03e7365263ad93fb8874e25788
juri-boni <100702839+juri-boni@users.noreply.github.com>
2023-01-18 20:14:03 +0100

    style --change post style

--- src/components/post/post.component.jsx
+++ src/components/post/post.component.jsx
@@ -12,13 +12,15 @@ const Post = ({ post }) => {
 
   return (
     <div className="post-container">
-      <div className="title-container">
+      <div className="title-body-container">
         <h2 className="post-title"> {title}</h2>
+        <p className="post-body">{body} </p>
+      </div>
+      <div className="remove-button-container">
         <span className="remove-button" onClick={deletePostHandler}>
           &#10005;
         </span>
       </div>
-      <p className="post-body">{body} </p>
     </div>
   );
 };
